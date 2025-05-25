fab07a57a3a83fbef83b823283cc194b02aa821e
wesley louis <louiswesley409@gmail.com>
2025-05-25 18:54:04 -0400

    adding method payment

--- haiti-virtual-mailbox-frontend/src/pages/Home.tsx
+++ haiti-virtual-mailbox-frontend/src/pages/Home.tsx
@@ -2,6 +2,7 @@ import "../styles/Home.css";
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import SupportModal from "../components/SupportModal"; // ✅ import modal
+import { toast } from "react-toastify";
 
 const Home = () => {
   const { t } = useTranslation();
@@ -10,6 +11,54 @@ const Home = () => {
     <div className="home-page">
       <h1>{t("home.title")}</h1>
       <p>{t("home.subtitle")}</p>
+      {/* 💰 Payment Methods */}
+      <div className="home-payment-methods">
+        <h3>💳 {t("home.paymentMethods")}</h3>
+        <div className="payment-grid">
+          <div className="payment-card">
+            <img src="/logos/Cash-App-Logo.png" alt="CashApp" />
+            <p>{t("home.cashapp")}: $haitibox</p>
+
+            <button
+              onClick={() => {
+                navigator.clipboard.writeText("$haitibox");
+                toast.success("✅ Copied CashApp ID!");
+              }}
+              className="copy-btn"
+            >
+              📋 Copy
+            </button>
+          </div>
+
+          <div className="payment-card">
+            <img src="/logos/Zelle-logo.png" alt="Zelle" />
+            <p>{t("home.zelle")}: bensleyrameau@gmail.com</p>
+            <button
+              onClick={() => {
+                navigator.clipboard.writeText("bensleyrameau@gmail.com");
+                toast.success("✅ Copied Zelle address!");
+              }}
+              className="copy-btn"
+            >
+              📋 Copy
+            </button>
+          </div>
+          <div className="payment-card">
+            <img src="/logos/Moncash-Logo.png" alt="MonCash" />
+            <p>{t("home.moncash")}: +509 3245 6789</p>
+
+            <button
+              onClick={() => {
+                navigator.clipboard.writeText("+509 3245 6789");
+                toast.success("✅  Copied MonCash number!");
+              }}
+              className="copy-btn"
+            >
+              📋 Copy
+            </button>
+          </div>
+        </div>
+      </div>
       <Link to="/register" className="btn">
         {t("home.getStarted")}
       </Link>
